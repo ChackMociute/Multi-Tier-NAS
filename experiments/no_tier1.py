7ab58235b599edcc62c33d61f0ca8014275c4ca1
unknown <martynasvaznonis@gmail.com>
2023-05-13 19:58:36 +0200

    Added ablation experiments

---
+++ experiments/no_tier1.py
@@ -0,0 +1,23 @@
+import os
+import json
+from utils import *
+from naslib import utils
+from warnings import filterwarnings
+
+filterwarnings('ignore')
+
+name = 'tier1absent'
+
+config = utils.load_config(os.path.join(os.getcwd(), 'config.yaml'))
+config.search.epochs = 200
+dataset_api = utils.get_dataset_api(search_space=config.search_space, dataset=config.dataset)
+tiers = [TrainingSpeedEstimateTier(dataset_api, config),
+         QueryFullTrainingTier(config.dataset, dataset_api)]
+
+results, averages = experiment(config, save_averages=True, tiers=tiers)
+
+path = os.path.join(os.getcwd(), 'results', config.search_space, config.dataset, name)
+if not os.path.exists(path): os.makedirs(path)
+with open(os.path.join(path, 'accuracies.json'), 'w') as f:
+    f.write(json.dumps(results))
+save_averages(averages, os.path.join(path, 'averages.json'))
---
+++ experiments/no_tier2.py
@@ -0,0 +1,24 @@
+import os
+import json
+from utils import *
+from naslib import utils
+from warnings import filterwarnings
+
+filterwarnings('ignore')
+
+name = 'tier2absent'
+
+config = utils.load_config(os.path.join(os.getcwd(), 'config.yaml'))
+config.search.epochs = 200
+train_loader = utils.get_train_val_loaders(config, mode="train")[0]
+dataset_api = utils.get_dataset_api(search_space=config.search_space, dataset=config.dataset)
+tiers = [JaCovTier(train_loader),
+         QueryFullTrainingTier(config.dataset, dataset_api)]
+
+results, averages = experiment(config, save_averages=True, tiers=tiers)
+
+path = os.path.join(os.getcwd(), 'results', config.search_space, config.dataset, name)
+if not os.path.exists(path): os.makedirs(path)
+with open(os.path.join(path, 'accuracies.json'), 'w') as f:
+    f.write(json.dumps(results))
+save_averages(averages, os.path.join(path, 'averages.json'))
